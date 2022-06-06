8bf5dec8920e7c26cf632db928c86ce981975710
Pralhad083 <pralhadphule11@gmail.com>
2022-06-06 09:41:33 +0530

    Add project files.

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstMVCApp.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Sponser()
+        {
+            return View();
+        }
+
+        public ActionResult Content()
+        {
+            return View();
+        }
+
+        public ActionResult Products()
+        {
+            return View();
+        }
+
+        //public string Find(string country,string state)
+        //{
+        //    return ("Country : " + country + " " + "State : " + state);
+        //}
+
+        public ActionResult Find(String Country, String State)
+        {
+            string result = "Country :- " + Country + "State :- " + State;
+            return Content(result);
+
+        }
+    }
+}
